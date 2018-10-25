1d4602cf2aeab78aead5b41810c7cdf9b151ebff
jerry1646 <jerrysong131@gmail.com>
2018-10-25 22:04:54 +0000

    sum array with recursive method

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for (let items of array){
+    if(Array.isArray(items)){
+      sum += sumItems(items);
+    } else{
+      sum += items;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
